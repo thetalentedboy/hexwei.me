7021652d0a03483384f22a142c38ab1224dc084c
hexwei <w17664062061@gamil.com>
2024-07-03 11:05:46 +0800

    fix: path

--- config/const.ts
+++ config/const.ts
@@ -2,4 +2,4 @@ import { Config } from "../etc/config"
 import { readYaml } from "../app/api/post/[[...slug]]/utils"
 import path from "path"
 
-export const config = readYaml<Config>(path.resolve("../etc/config.yaml"))
+export const config = readYaml<Config>(path.resolve("./etc/config.yaml"))
